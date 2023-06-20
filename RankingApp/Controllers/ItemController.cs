c5e798e9201852ec08d52b6a2cd33c1bbac02eea
Vidur Agarwal <geurecruitviduragarwal@gmail.com>
2023-06-20 13:54:49 +0530

    Commmit

---
+++ RankingApp/Controllers/ItemController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RankingApp.Models;
+
+namespace RankingApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : ControllerBase
+    {
+        private static readonly IEnumerable<ItemModel> Items = new[]
+        {
+            new ItemModel{ Id = 1, Title = "The Godfather", ImageId = 1, Ranking = 0, ItemType = 1},
+            new ItemModel{ Id = 2, Title = "Highlander", ImageId = 2, Ranking = 0, ItemType = 1},
+            new ItemModel{ Id = 3, Title = "Highlander2", ImageId = 3, Ranking = 0, ItemType = 1},
+            new ItemModel{ Id = 4, Title = "The last of mohicans", ImageId = 4, Ranking = 0, ItemType = 1},
+            new ItemModel{ Id = 5, Title = "Police academy 6", ImageId = 5, Ranking = 0, ItemType = 1},
+            new ItemModel{ Id = 6, Title = "Rear window", ImageId = 6, Ranking = 0, ItemType = 1},
+            new ItemModel{ Id = 7, Title = "Road house", ImageId = 7, Ranking = 0, ItemType = 1},
+            new ItemModel{ Id = 8, Title = "Shawshank Redemption", ImageId = 8, Ranking = 0, ItemType = 1},
+            new ItemModel{ Id = 9, Title = "Startrek 4", ImageId = 9, Ranking = 0, ItemType = 1},
+            new ItemModel{ Id = 10, Title = "Superman 4", ImageId = 10, Ranking = 0, ItemType = 1}
+        };
+        [HttpGet("{itemType:int}")]
+        public ItemModel[] Get(int itemType)
+        {
+            ItemModel[] items = Items.Where(i=>i.ItemType == itemType).ToArray();
+            System.Threading.Thread.Sleep(2000);
+            return items;
+        }
+    }
+}
